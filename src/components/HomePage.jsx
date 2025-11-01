c9e1d8256edd9b75ccc1754ff35ac90c0eaf32f3
shrijithps <p.shrijith@talview.com>
2025-11-01 15:59:37 +0530

    removed_console_log

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -14,7 +14,6 @@ export default function HomePage() {
         useEffect(() => {
             dispatch(fetchTrendingRequest());
         }, [dispatch]);
-        console.log("trending videos", trendingVideos);
 
     return (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
